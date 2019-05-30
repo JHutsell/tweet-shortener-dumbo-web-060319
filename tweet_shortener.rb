032a6d8aa552143598c41166f4012e8d53c33e02
Justin Hutsell <hutselljustin@gmail.com>
2019-05-30 21:17:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -33,6 +33,6 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  word_substituter(tweet) if tweet.length > 140
-  tweet.length > 140?
+  # word_substituter(tweet) if tweet.length > 140
+  tweet.length > 140 ? word_substituter(tweet) : tweet
 end
